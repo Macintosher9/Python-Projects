db2c7cf9dcf6b7b9731a4913a7c768ec704d0d13
Mac <macchang09@gmail.com>
2025-01-25 10:35:12 -0500

    Create Crash Course.py

---
+++ Crash Course.py
@@ -0,0 +1,8 @@
+# Here is what I learn from my new book call Python Crash course by Eric Matthes
+firstname="mac"
+lastname="chang"
+fullname=f"{firstname} {lastname}"
+print(f"Hello, {fullname.title()}!\n")
+
+# The \n\t is to indent and add whitspace between each word
+print("To do list:\n\tLaundry\n\tCleaning my room\n\tOthers")
