8feb6aa54fc55241d6c0f33a0f9e69a95c4c6288
Mac <macchang09@gmail.com>
2025-01-25 14:42:06 -0500

    Update Temperature Converter.py

--- Temperature Converter.py
+++ Temperature Converter.py
@@ -1,9 +1,11 @@
-temp1=input("Would you like convert your temperature into celsius or fahrenheit?")
-if temp1 == "celsius" or "Celsius":
-    fahreheit1 = int(input("What tempeature would you like to convert to celsius?"))
-    celsius1 = ((fahreheit1-32)*(5/9))
-    print(fahreheit1,"degree in celsius is",celsius1,"degree fahrenheit")
-elif temp1 == "fahrenheit" or "Fahrenheit":
-    celsius2 = int(input("What temperature would you like to convert to fahrenheit?"))
-    fahreheit2 = ((celsius2*5/9)+32)
-    print(celsius2,"degree in fahreheit is", fahreheit2,"degree celsius")
+askuser1=input("Would you like you temperature convert to Celsius or Fahreheit? ")
+ 
+if askuser1=="fahreheit":
+    C=int(input("What would you like to convert to fahreheit?\n"))
+    F=((C*9/5)+32)
+    print(C,"Celsius is",F,"Fahrehiet")
+
+elif askuser1=="celsius":
+    F=int(input("What would you like to convert to celsius?\n"))
+    C=int((F-32)*5/9)
+    print(F,"Fahreheit is",C,"Celsius")
