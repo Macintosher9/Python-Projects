dc1f0e58db8abafd68ac632616a0aca7f55918c4
Mac <macchang09@gmail.com>
2025-01-29 16:28:23 -0500

    Create Temperature Converter.py

---
+++ Temperature Converter.py
@@ -0,0 +1,11 @@
+askuser1=input("Would you like you temperature convert to Celsius or Fahreheit? ")
+ 
+if askuser1=="fahreheit":
+    C=int(input("What would you like to convert to fahreheit?\n"))
+    F=((C*9/5)+32)
+    print(C,"Celsius is",F,"Fahrehiet")
+
+elif askuser1=="celsius":
+    F=int(input("What would you like to convert to celsius?\n"))
+    C=int((F-32)*5/9)
+    print(F,"Fahreheit is",C,"Celsius")
