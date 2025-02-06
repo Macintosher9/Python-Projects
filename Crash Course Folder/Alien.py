e5367c58aa119e7fd49a373abfd2f9a48e2a08d9
Macintosher9 <macchang09@gmail.com>
2025-02-06 08:07:51 -0500

    git add

---
+++ Crash Course Folder/Alien.py
@@ -0,0 +1,10 @@
+alien_0 = {'color': 'green'
+        ,'speed':'slow'
+        }
+print(alien_0['color'])
+if alien_0['color']=='green':
+    print('You earned 5 points')
+elif alien_0['color']=='yellow':
+    print('You earned 10 points')
+else:
+    print('You earn 15 points')
--- Crash Course Folder/Alien_Color.py
+++
@@ -1,7 +0,0 @@
-alien_color='red'
-if alien_color=='green':
-    print('You earned 5 points')
-elif alien_color=='yellow':
-    print('You earned 10 points')
-else:
-    print('You earn 15 points')
