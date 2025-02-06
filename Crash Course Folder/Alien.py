4c96bc47b945e4bd2e056fd391a5dad81d503a1f
Macintosher9 <macchang09@gmail.com>
2025-02-06 08:13:48 -0500

    git add

--- Crash Course Folder/Alien.py
+++ Crash Course Folder/Alien.py
@@ -1,10 +1,21 @@
-alien_0 = {'color': 'green'
-        ,'speed':'slow'
-        }
-print(alien_0['color'])
-if alien_0['color']=='green':
-    print('You earned 5 points')
-elif alien_0['color']=='yellow':
-    print('You earned 10 points')
-else:
-    print('You earn 15 points')
+def alienpoints():
+    alien_0 = {'color': 'green'
+            ,'speed':'slow'
+            }
+    print(alien_0['color'])
+    if alien_0['color']=='green':
+        print('You earned 5 points')
+    elif alien_0['color']=='yellow':
+        print('You earned 10 points')
+    else:
+        print('You earn 15 points')
+
+def multiplealien():
+    alien_0={'color':'green','points':5}
+    alien_1={'color':'yellow','points':10}
+    alien_2={'color':'red','points':15}
+    aliens=[alien_0,alien_1,alien_2]
+    for alien in aliens:
+        print(alien)
+
+multiplealien()
