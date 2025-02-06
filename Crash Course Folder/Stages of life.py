2c0282f0f3b74ab6b077672096d8bd0911c62d9f
Macintosher9 <macchang09@gmail.com>
2025-02-05 22:41:57 -0500

    git add

---
+++ Crash Course Folder/Stages of life.py
@@ -0,0 +1,14 @@
+personAge=20
+
+if personAge<2:
+    print('This person is a baby')
+elif personAge>=2 and personAge<4:
+    print('This person is a toddler')
+elif personAge>=4 and personAge<13:
+    print('This person is a kid')
+elif personAge>=13 and personAge<20:
+    print('This person is a Teenager')
+elif personAge>=20 and personAge<65:
+    print('This person is an adult')
+else:
+    print('This person is an elder')
