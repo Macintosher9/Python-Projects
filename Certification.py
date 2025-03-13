f2ea639a04d597dd98a4d7273223d4cd421d26d7
Macintosher9 <macchang09@gmail.com>
2025-03-13 15:23:14 -0400

    update

---
+++ Certification.py
@@ -0,0 +1,10 @@
+username = "Macintosher09"
+password = "Lemonsqueeze"
+
+usernamenter = input("What is your username? ")
+passwordenter = input("What is your passoword? ")
+
+if usernamenter != "Macintosher09" and passwordenter != "Lemonsqueeze":
+    print("Access Denied")
+else:
+    print("Access Granted")
