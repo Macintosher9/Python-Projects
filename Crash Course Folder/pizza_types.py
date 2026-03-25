2f59f926f10f3c3edcf468df3e2da62648185679
Macintosher <macchang09@gmail.com>
2026-03-24 22:28:39 -0400

    git commit -m "Deleted old files and added new ones to the project."

--- Crash Course Folder/pizza_types.py
+++
@@ -1,4 +0,0 @@
-pizzas = ['Meat Lover', 'Pepperoni', 'Sausage']
-for pizza in pizzas:
-    print("I like",pizza)
-print('I really like',pizzas[0])
--- README.md
+++
@@ -1,3 +0,0 @@
-In ths folder I put:
-<p>A Temperature converter
-<p></p>Stuff from my book Python Crash Course
