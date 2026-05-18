b74be5c84098e54d1f8fc443da5dc93b20867994
Macintosher <macchang09@gmail.com>
2026-05-18 17:14:56 -0400

    Got to get screen to stay open

--- testing.py
+++ testing.py
@@ -3,18 +3,19 @@ import sys
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Detecting Key inputs")
+screen = pygame.display.set_mode((800,800))
+pygame.display.set_caption(("Hello World"))
+Background = (255,255,255)
 
-WHITE = (255, 255, 255)
 running = True
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.quit():
+        if event.type == pygame.QUIT:
             running = False
-
-    screen.fill(WHITE)
+            pygame.quit()
+        
+    screen.fill(Background)
     pygame.display.flip()
-
+    
 pygame.quit()
 sys.exit()
