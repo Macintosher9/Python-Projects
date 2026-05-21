b00a5feff2f5dcea7a9dc934a3f169b2ad3d8061
Macintosher <macchang09@gmail.com>
2026-05-20 21:52:12 -0400

    added fonts and making a music volume slider

--- AP_CSP_Final.py
+++ AP_CSP_Final.py
@@ -1,30 +1,33 @@
 import pygame
 import sys
+from pygame import mixer
 pygame.init()
+pygame.font.init()
 
 screen = pygame.display.set_mode((1280,720))
 pygame.display.set_caption("AP CSP Final Project")
 
-width = screen.get_width
-height = screen.get_height
-background = (196, 196, 196)
+width = screen.get_width()
+height = screen.get_height()
 
-buttonFont = pygame.font.Font('Vogue.ttf', 50)
-Txt1 = buttonFont.render('Press Here', True, (0,0,0))
-Txtrect = Txt1.get_rect()
-Txtrect.center = (640, 360)
+background = (196, 196, 196)
+black = (0,0,0)
+
+musicFont = pygame.font.SysFont("Avenis", 50)
+musicTxt = musicFont.render("Music", True, (0,0,0))
+musicRect = musicTxt.get_rect(center=(width/2, height/2-50))
+pygame.draw.line(screen, black, (440, 360), (840, 360), 5)
 
 running = True
 while running:
+    
+    screen.fill(background)
+    screen.blit(musicTxt, musicRect)
+    
     for ev in pygame.event.get():
         if ev.type == pygame.QUIT:
             running = False
         
-        screen.fill(background)
-        pygame.draw.rect(screen, (82, 88, 209), [440, 310, 400, 100], 0)
-        screen.blit(Txt1, Txtrect)
-        pygame.display.update()
-        
-        
+    pygame.display.flip()
 pygame.quit()
 sys.exit()
