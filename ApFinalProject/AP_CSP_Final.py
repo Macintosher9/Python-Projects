ff259b84d503cbd7451e85d8211708af384fd3a2
Macintosher <macchang09@gmail.com>
2026-06-02 14:07:29 -0400

    Added classes and moved around a few things

--- ApFinalProject/AP_CSP_Final.py
+++ ApFinalProject/AP_CSP_Final.py
@@ -4,75 +4,110 @@ from pygame import mixer
 pygame.init()
 pygame.font.init()
 pygame.mixer.init()
-mixer.music.load("jazzCafe.mp3")
-mixer.music.set_volume(0.0)
-mixer.music.play(-1)
 
-# Screen
-screen = pygame.display.set_mode((1280,720))
-ground = pygame.Rect((0, 540), (1280, 30))
-pygame.display.set_caption("AP CSP Final Project")
-
-width = screen.get_width()
-height = screen.get_height()
-
+# Screen Setup
+screenwidth = 1440
+screenheight = 720
+screen = pygame.display.set_mode((screenwidth, screenheight))
 # Colors
-background = (196, 196, 196)
-black = (0,0,0)
-grey = (58,58,58)
+Black = (0,0,0)
+gray1 = (82,82,82)
+gray2 = (140,140,140)
+gray3 = (183,183,183)
+gray4 = (232,232,232)
 
-# Music Settings Stuff
-musicFont = pygame.font.Font("roboto\\Roboto-Black.ttf", 50)
-musicTxt = musicFont.render("Music", True, (0,0,0))
-musicRect = musicTxt.get_rect(center=(width/2, height/2-50))
-volFont = pygame.font.Font("roboto\\Roboto-Black.ttf", 20)
-track = pygame.Rect((540, 360), (200, 10))
-knob = pygame.Rect((540,345), (20,20))
-knobR = 10
-knob.centery = track.centery
+# Music Setup
+mFont = pygame.font.Font("roboto\\Roboto-Medium", 48)
+mTxt = mFont.render("Music Volume", True, Black)
+mRect = mTxt.get_rect(center=(screen//2, 80))
+vFont = pygame.font.Font("roboto\\Roboto-Medium", 28)
 
-# Player Properties
-player = pygame.Rect()
-playerRad = 15
-
-track_value = 0.0
-running = True
-dragging = False
-
-while running:
-    for ev in pygame.event.get():
-        
-        if ev.type == pygame.QUIT:
-            running = False
-        
+class slider:
+    def __init__(self, x, y, width):
+        self.track = pygame.Rect((x, y), (width, 10))
+        self.knob = pygame.Rect((x, y-15), (20, 20))
+        self.knobR = 10
+        self.knob.centery = self.track.centery
+        self.dragging = False
+        self.vol = 0.5
+        self.knob.centerx = self.track.left + int(self.vol*self.track.width)
+    
+    def handlevent(self, ev):
+        if ev.type == pygame.MOUSEBUTTONDOWN:
+            self.dragging = True
         elif ev.type == pygame.MOUSEBUTTONDOWN:
-            if ev.button == 1:
-                if knob.collidepoint(ev.pos):
-                    dragging = True
-        elif ev.type == pygame.MOUSEBUTTONUP:
-            if ev.button == 1:
-                dragging = False
+            self.dragging = False
         elif ev.type == pygame.MOUSEMOTION:
-            if dragging:
-                knob.centerx = max(track.left, min(ev.pos[0], track.right))
-                track_value = (knob.centerx - track.left) / track.width
-                mixer.music.set_volume(track_value)
-                print(f"Volume: {track_value:.2f}", end="\r")
-    
-    screen.fill(background)
-    pygame.draw.rect(screen, black, ground)
-    
-    def musicRoom():
-        screen.blit(musicTxt, musicRect)
-    
-        pygame.draw.rect(screen, grey, track)
-        pygame.draw.circle(screen, black, knob.center, knobR)
-    
-        perNum = int(track_value*100)
-        pertxt = volFont.render(f"{perNum}%", True, black)
-        screen.blit(pertxt, (track.right +15, track.centery-10))
-    
-    musicRoom()
-    pygame.display.flip()
-pygame.quit()
-sys.exit()
+            if self.dragging:
+                self.knob.centerx = max(self.track.left, min(ev.pos[0], self.track.right))
+                self.vol = (self.knob.centerx - self.track.right) / self.track.width
+    def draw(self, surface):
+        pygame.draw.rect(surface, gray3, self.track)
+        pygame.draw.circle(surface, Black, self.knob.center, self.knobR)
+        numPer = int(self.vol * 100)
+        txtPer = vFont.render(f"{numPer}%", True, Black)
+        surface.blit(txtPer, (self.track.right + 15, self.track.centery - 12))
+
+class level:
+    def __init__ (self, mapLayout, tileSize, blockColor, bgColor)
+        self.mapyLayout = mapLayout
+        self.tileSize = tileSize
+        self.blockColor = blockColor
+        self.bgColor = bgColor
+        self.platform = []
+        self.generateLvl()
+    def generateLvl(self):
+        for rowInd, row in enumerate(self.mapyLayout):
+            for colInd, cell in enumerate(row):
+                if cell == "X":
+                    x = colInd * self.tileSize
+                    y = rowInd * self.tileSize
+                    rect = pygame.Rect(x, y, self.tileSize, self.tileSize)
+                    self.platform.append(rect)
+    def draw(self, surface):
+        for platform in self.platform:
+            pygame.draw.rect(surface, self.blockColor, platform)
+
+class Player:
+    def __init__ (self, x, y):
+        self.rect = pygame.Rect(x, y, 40, 60)
+        self.speed = 7
+        self.xVal = 0
+        self.yVal = 0
+        self.grav = 0.8
+        self.jumpStrength = -16
+        self.grounded = False
+    def update(self, platforms):
+        keys = pygame.key.get_just_pressed
+        self.xVal = 0
+        if keys[pygame.K_LEFT] or [pygame.K_a]
+            self.xVal = -self.speed
+        if keys[pygame.K_RIGHT] or [pygame.k_d]:
+            self.xval = self.speed
+        if (keys[pygame.K_SPACE] or keys[pygame.K_w]) and self.grounded:
+            self.yVal += self.jumpStrength
+            self.grounded = False
+        
+        self.yVal += self.grav
+        self.rect.x += self.xVal
+        for platform in platforms:
+            if self.rect.collidepoint(platform):
+                if self.xVal > 0: self.rect.right == platform.left
+                if self.xVal < 0: self.rect.left == platform.right
+        
+        self.rect.y += self.yVal
+        self.grounded = False
+        for platform in platforms:
+            # for Falling
+            if self.yVal > 0:
+                self.rect.bottom = platform.top
+                self.yVal = 0
+                self.grounded = True
+            # for jumping
+            if self.yVal < 0:
+                self.rect.top = self.rect.bottom
+                self.yVal = 0
+    def draw(self, surface):
+        pygame.draw.rect(surface, gray3, self.rect)
+
+volSlider = Slider(540, 140, 200)
